23111f0e776166579e6bfc82158d6d15f813d488
nabeel khan <nabeel4khan@gmail.com>
2023-03-29 00:45:38 +0530

    changed code to add to choose random pic

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const reviews = [
         id: 2,
         author: "Bhallu biryani",
         job: 'Backend developer',
-        pics: 'pics/nabeel.jpg',
+        pics: 'pics/pic1.jfif',
         info: "Ipsam nulla eveniet consequatur possimus architecto hic, Lorem ipsum dolor sit, amet consectetur adipisicing elit. facere corrupti aspernatur officiis voluptatibus nostrum repellendus quisquam voluptas est dolore illo eum iste perferendis."
     },
     {
         id: 3,
         author: "Sarah james",
         job: 'Graphics designer',
-        pics: 'pics/nabeel.jpg',
+        pics: 'pics/pic2.jfif',
         info: "voluptatibus nostrum repellendus quisquam voluptas est dolore illo eum iste perferendis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam nulla eveniet consequatur possimus architecto hic, facere corrupti aspernatur officiis "
     },
     {
         id: 4,
         author: "Waseem bhai",
         job: 'Android developer',
-        pics: 'pics/nabeel.jpg',
+        pics: 'pics/WIN_20221126_15_38_55_Pro.jpg',
         info: "facere corrupti aspernatur officiis voluptatibus nostrum repellendusLorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam nulla eveniet consequatur possimus architecto hic,  quisquam voluptas est dolore illo eum iste perferendis."
     }
 ]
@@ -37,8 +37,8 @@ let info = document.getElementById('info');
 let prevbtn = document.querySelector('.prev-btn');
 let nextbtn = document.querySelector('.next-btn');
 let citem= 0;
-// let random = Math.floor(Math.random()*3);
-window.addEventListener('DOMContentLoaded', changeview(0));
+let random = Math.floor(Math.random()*3);
+window.addEventListener('DOMContentLoaded', changeview(random));
 
 prevbtn.addEventListener('click', ()=>{
     if(citem == 0){
@@ -60,7 +60,7 @@ function changeview(number){
     job.innerHTML = item.job;
     info.innerHTML = item.info;
     author.innerHTML = item.author;
-    // console.log('asd');
+    img.src = item.pics;
 }
 
 setInterval(()=>{
